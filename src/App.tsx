973f859e1bd2e47b7e787df3c3b9bb1e97279277
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-12-17 16:26:06 +0530

    changes :  - Calendars are mapped

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Calendar from "./Components/Calendar/Calendar"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-3xl my-4 ">Calendar 2023</h1>
+      {
+        [...Array(12).keys()].map((month , index)=>{
+          return <Calendar key={index} year={2023} month={month} />
+        })
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Calendar/Calendar.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import classNames from 'classnames';
+import months from '../../data/MonthName';
+
+interface CalendarProps {
+    year: number;
+    month: number;
+}
+
+const daysInMonth = (month: number, year: number): Date => {
+    return new Date(year, month + 1, 0)
+};
+
+const Calendar: React.FC<CalendarProps> = ({ year, month }) => {
+
+    const totalDays: number = daysInMonth(month, year).getDate();
+    const firstDayOfMonth: number = new Date(year, month, 1).getDay();
+    const currentMonth = months[month];
+
+    //   const weeks : number = Math.ceil((totalDays + firstDayOfMonth) / 7);
+    const daysArray: number[] = [];
+
+    // Populate daysArray with day numbers
+    for (let i = 1; i <= totalDays; i++) {
+        daysArray.push(i);
+    }
+
+    return (
+        <div className="w-full max-w-md mx-auto p-4 border border-black my-1 rounded-md shadow-md ">
+            <p className='text-xl font-bold text-black text-center my-2 '>{currentMonth}</p>
+            <div className="grid grid-cols-7 gap-1 text-center">
+                {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => (
+                    <div key={day} className="text-sm font-medium text-gray-600">
+                        {day}
+                    </div>
+                ))}
+            </div>
+            <div className="grid grid-cols-7 gap-1">
+                {/* Render empty cells for days before the first day of the month */}
+                {Array.from({ length: firstDayOfMonth }, (_, i) => (
+                    <div key={`empty-${i}`} className="text-center"></div>
+                ))}
+                {/* Render days */}
+                {daysArray.map(day => {
+                    const currentDate = new Date()
+                    
+                    return <div
+                        key={day}
+                        className={classNames(
+                            'text-center py-2 rounded',
+                            {
+                                'bg-blue-500 text-white ': (day == currentDate.getDate() && month == currentDate.getMonth()) ,
+                                'text-gray-400': day === 0,
+                                'bg-gray-200': day % 2 === 0,
+                                'bg-gray-300': day % 2 !== 0,
+                            },
+                            'text-gray-900'
+                        )}
+                    >
+                        {day}
+                    </div>
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Calendar;
