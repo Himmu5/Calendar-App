0da7febd873e47a51c896d8be05b12d08a70a25d
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-12-17 19:11:52 +0530

    ..

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -58,9 +58,8 @@ const Calendar: React.FC<CalendarProps> = ({ year, month }) => {
                                 'text-gray-400': day === 0,
                                 'bg-gray-200': day % 2 === 0,
                                 'bg-gray-300': day % 2 !== 0,
-                                ' bg-blue-500 text-white ' : highlight
                             },
-                            'text-gray-900'
+                            'text-gray-900 ' + ( highlight ? 'bg-blue-500 text-white' : '') 
                         )}
                     >
                         {day}
